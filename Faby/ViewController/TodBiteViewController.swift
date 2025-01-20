a65eb2d1ee4ded417cebbeaabc2568708736599d
Deepak  Prajapati <126603027+deepakstwt@users.noreply.github.com>
2025-01-20 15:02:21 +0530

    todbite commit

--- Faby/ViewController/TodBiteViewController.swift
+++ Faby/ViewController/TodBiteViewController.swift
@@ -9,13 +9,30 @@ import UIKit
 
 class TodBiteViewController: UIViewController {
 
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBOutlet weak var CollectionView: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        let nib = UINib(nibName: "TodBitCollectionViewCell", bundle: nil)
+        CollectionView.register(nib, forCellWithReuseIdentifier: "cell")
     }
     
 
-   
+    @IBAction func segmentedControlTapped(_ sender: UISegmentedControl) {
+        let select = segmentedControl.selectedSegmentIndex
+        switch select{
+        case 0:
+            CollectionView.isHidden = false
+        case 1:
+            CollectionView.isHidden = true
+        default :
+            CollectionView.isHidden = false
+        }
+    
+        
+    }
+    
 
 }
