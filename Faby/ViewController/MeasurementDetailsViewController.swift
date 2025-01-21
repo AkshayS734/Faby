2ecf2dd483d6305cfaa417195f278b27a8417a27
Akshay Shukla <manasshukla876@gmail.com>
2025-01-21 22:34:10 +0530

    graph working properly

--- Faby/ViewController/MeasurementDetailsViewController.swift
+++ Faby/ViewController/MeasurementDetailsViewController.swift
@@ -1,5 +1,6 @@
 import UIKit
 import SwiftUI
+
 class MeasurementDetailsViewController: UIViewController {
     @IBOutlet weak var timeSpanSegmentedControl: UISegmentedControl!
     
@@ -12,73 +13,102 @@ class MeasurementDetailsViewController: UIViewController {
     var isUnitChanged: Bool = false
     
     var unitSettings = UnitSettingsViewModel()
-    
     var baby: Baby? {
         return BabyDataModel.shared.babyList.first
     }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
-        print("\(measurementType!)")
         title = measurementType
         
+        configureSegmentedControl()
+        setupDataForTimeSpan()
+        embedSwiftUIView()
+    }
+    
+    private func configureSegmentedControl() {
         timeSpanSegmentedControl.removeAllSegments()
         let segments = ["Week", "Month", "6 Months", "Year"]
         for (index, title) in segments.enumerated() {
             timeSpanSegmentedControl.insertSegment(withTitle: title, at: index, animated: false)
-            }
+        }
         timeSpanSegmentedControl.selectedSegmentIndex = 3
-        setupDataForTimeSpan()
-        embedSwiftUIView()
-        
     }
+    
     @IBAction func timeSpanChanged(_ sender: UISegmentedControl) {
         switch sender.selectedSegmentIndex {
-            case 0: selectedTimeSpan = "Week"
-            case 1: selectedTimeSpan = "Month"
-            case 2: selectedTimeSpan = "6 Months"
-            case 3: selectedTimeSpan = "Year"
-            default: selectedTimeSpan = "Year"
+        case 0: selectedTimeSpan = "Week"
+        case 1: selectedTimeSpan = "Month"
+        case 2: selectedTimeSpan = "6 Months"
+        case 3: selectedTimeSpan = "Year"
+        default: selectedTimeSpan = "Year"
         }
         setupDataForTimeSpan()
         embedSwiftUIView()
     }
+    
     private func setupDataForTimeSpan() {
-        guard let measurementType = measurementType else { return }
-        guard let baby = BabyDataModel.shared.babyList.first else { return }
+        guard let measurementType = measurementType, let baby = baby else { return }
         
-        switch measurementType {
-        case "Height":
-            currentGrowthData = Array(baby.height.keys).sorted()
-            currentTimeLabels = Array(baby.height.values).map {
-                DateFormatter.localizedString(from: $0, dateStyle: .short, timeStyle: .none)
-            }
-        case "Weight":
-            currentGrowthData = Array(baby.weight.keys).sorted()
-            currentTimeLabels = Array(baby.weight.values).map {
-                DateFormatter.localizedString(from: $0, dateStyle: .short, timeStyle: .none)
-            }
-        case "Head Circumference":
-            currentGrowthData = Array(baby.headCircumference.keys).sorted()
-            currentTimeLabels = Array(baby.headCircumference.values).map {
-                DateFormatter.localizedString(from: $0, dateStyle: .short, timeStyle: .none)
-            }
+        let calendar = Calendar.current
+        let today = Date()
+        
+        switch selectedTimeSpan {
+        case "Week":
+            let startOfWeek = calendar.date(byAdding: .day, value: -6, to: today)!
+            filterData(from: startOfWeek, to: today)
+            
+        case "Month":
+            let startOfMonth = calendar.date(byAdding: .month, value: -1, to: today)!
+            filterData(from: startOfMonth, to: today)
+            
+        case "6 Months":
+            let startOfSixMonths = calendar.date(byAdding: .month, value: -6, to: today)!
+            filterData(from: startOfSixMonths, to: today)
+            
+        case "Year":
+            let startOfYear = calendar.date(byAdding: .year, value: -1, to: today)!
+            filterData(from: startOfYear, to: today)
+            
         default:
-            break
+            filterData(from: .distantPast, to: today)
         }
         
         if isUnitChanged {
             convertDataToSelectedUnits()
         }
     }
+    
+    private func filterData(from startDate: Date, to endDate: Date) {
+        guard let baby = baby else { return }
+        
+        var allData: [Double: Date] = [:]
+        switch measurementType {
+        case "Height":
+            allData = baby.height
+        case "Weight":
+            allData = baby.weight
+        case "Head Circumference":
+            allData = baby.headCircumference
+        default:
+            break
+        }
+        
+        let filteredData = allData.filter { $0.value >= startDate && $0.value <= endDate }
+        currentGrowthData = filteredData.keys.sorted()
+        currentTimeLabels = filteredData.values.sorted().map {
+            DateFormatter.localizedString(from: $0, dateStyle: .short, timeStyle: .none)
+        }
+    }
+    
     private func convertDataToSelectedUnits() {
-            // Conversion logic if necessary
         if selectedHeightUnit == "inches" {
             currentGrowthData = currentGrowthData.map { $0 * 0.393701 }
         } else if selectedHeightUnit == "cm" {
             currentGrowthData = currentGrowthData.map { $0 / 0.393701 }
         }
-
+        
         if selectedWeightUnit == "lbs" {
             currentGrowthData = currentGrowthData.map { $0 * 2.20462 }
         } else if selectedWeightUnit == "kg" {
@@ -86,48 +116,49 @@ class MeasurementDetailsViewController: UIViewController {
         }
         isUnitChanged = false
     }
-
+    
     private func embedSwiftUIView() {
         guard let measurementType = measurementType else { return }
-
+        
         for subview in view.subviews {
             if let hostingController = subview.next as? UIHostingController<MeasurementDetailsView> {
                 hostingController.view.removeFromSuperview()
                 hostingController.removeFromParent()
             }
         }
-        let baby = BabyDataModel.shared.babyList.first
+        let baby = self.baby
         let swiftUIView = MeasurementDetailsView(
             measurementType: measurementType,
-            baby: baby
+            baby: baby,
+            currentGrowthData: currentGrowthData,
+            currentTimeLabels: currentTimeLabels
         )
         .environmentObject(unitSettings)
-                    
+        
         let hostingController = UIHostingController(rootView: swiftUIView)
         addChild(hostingController)
         hostingController.view.frame = view.bounds
         hostingController.view.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(hostingController.view)
-                        
+        
         NSLayoutConstraint.activate([
             hostingController.view.topAnchor.constraint(equalTo: timeSpanSegmentedControl.bottomAnchor, constant: 16),
             hostingController.view.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
             hostingController.view.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16)
         ])
-                        
+        
         hostingController.didMove(toParent: self)
     }
-        
+    
     @IBAction func addButtonTapped(_ sender: UIBarButtonItem) {
         presentMeasurementInputView()
     }
-    func presentMeasurementInputView() {
+    
+    private func presentMeasurementInputView() {
         let saveMeasurement: (String, Date) -> Void = { [weak self] inputMeasurement, date in
             guard let self = self, let baby = self.baby else { return }
-                    
-                    // Convert the input measurement to Double
+            
             if let measurement = Double(inputMeasurement) {
-                        // Update the corresponding measurement for the Baby object
                 switch self.measurementType {
                 case "Height":
                     baby.updateHeight(measurement, date: date)
@@ -138,20 +169,15 @@ class MeasurementDetailsViewController: UIViewController {
                 default:
                     break
                 }
-                        
-                        // You can reload data here if necessary
             }
         }
-                
-                // Create the MeasurementInputView with the closure for saving the measurement
+        
         let measurementInputView = MeasurementInputView(
             measurementType: measurementType ?? "",
             saveMeasurement: saveMeasurement
         )
-                
         let hostingController = UIHostingController(rootView: measurementInputView)
         hostingController.modalPresentationStyle = .formSheet
         present(hostingController, animated: true, completion: nil)
     }
-    
 }
--- Faby/ViewController/UnitSettingViewModel.swift
+++ Faby/ViewController/UnitSettingViewModel.swift
@@ -1,7 +1,7 @@
 import SwiftUI
 
 class UnitSettingsViewModel: ObservableObject {
-    @Published var selectedUnit: String = "Metric"
+    @Published var selectedUnit: String = "cm"
     @Published var weightUnit: String = "kg"
     @Published var isUnitChanged: Bool = false
 }
