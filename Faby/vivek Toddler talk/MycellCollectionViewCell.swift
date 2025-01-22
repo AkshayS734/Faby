10b28afcbc89804839e299018d7d35b7da49d70e
vivek kumar <vk092731@gmail.com>
2025-01-22 10:03:16 +0530

    toddler talk initial commit

---
+++ Faby/vivek Toddler talk/MycellCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  MycellCollectionViewCell.swift
+//  talk
+//
+//  Created by Batch - 1 on 18/01/25.
+//
+
+import UIKit
+
+class MycellCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var subtitle: UILabel!
+    @IBOutlet var title: UILabel!
+}
