d7cae946bc3eedffedf77ccccb37a38eb17ef1dd
Akshay Shukla <manasshukla876@gmail.com>
2025-01-29 00:46:48 +0530

    UI and logic improvements

--- Faby/CollectionView/SpecialMomentsCollectionViewCell.swift
+++ Faby/CollectionView/SpecialMomentsCollectionViewCell.swift
@@ -1,7 +1,7 @@
 import UIKit
 
 class SpecialMomentsCollectionViewCell: UICollectionViewCell {
-
+    
     @IBOutlet weak var specialMomentTitle: UILabel!
     @IBOutlet weak var specialMomentCategory: UILabel!
     @IBOutlet weak var specialMomentDate: UILabel!
@@ -9,11 +9,12 @@ class SpecialMomentsCollectionViewCell: UICollectionViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
         setupUI()
+        setupImageConstraints()
     }
     private func setupUI() {
         specialMomentsImage.layer.cornerRadius = 10
         specialMomentsImage.clipsToBounds = true
-        specialMomentsImage.contentMode = .scaleAspectFill
+        specialMomentsImage.contentMode = .scaleToFill
         
         specialMomentTitle.font = UIFont.systemFont(ofSize: 16, weight: .semibold)
         specialMomentTitle.textColor = .darkText
@@ -24,17 +25,28 @@ class SpecialMomentsCollectionViewCell: UICollectionViewCell {
         specialMomentDate.font = UIFont.systemFont(ofSize: 12, weight: .light)
         specialMomentDate.textColor = .lightGray
     }
-    
+    private func setupImageConstraints() {
+        specialMomentsImage.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            specialMomentsImage.widthAnchor.constraint(equalToConstant: 201),
+            specialMomentsImage.heightAnchor.constraint(equalToConstant: 150)
+        ])
+    }
     func configure(with milestone: (GrowthMilestone, Date)) {
         let (milestoneDetail, achievedDate) = milestone
+        
         specialMomentTitle.text = milestoneDetail.title
         specialMomentCategory.text = milestoneDetail.category.rawValue.capitalized
-        specialMomentDate.text = achievedDate.formatted()
-        if let userImage = milestoneDetail.userImagePath.flatMap({ UIImage(contentsOfFile: $0) }) {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .medium
+        specialMomentDate.text = dateFormatter.string(from: achievedDate)
+        if let userImagePath = milestoneDetail.userImagePath,
+           let userImage = UIImage(contentsOfFile: userImagePath) {
             specialMomentsImage.image = userImage
         } else {
             specialMomentsImage.image = UIImage(named: milestoneDetail.image)
         }
     }
-
+    
 }
