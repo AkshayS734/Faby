10cd98f72937553f4a9aef3b6e036c245b47c13f
Akshay Shukla <manasshukla876@gmail.com>
2025-02-13 15:54:05 +0530

    Video functionality improvements

--- Faby/CollectionView/SpecialMomentsCollectionViewCell.swift
+++ Faby/CollectionView/SpecialMomentsCollectionViewCell.swift
@@ -5,79 +5,111 @@ class SpecialMomentsCollectionViewCell: UICollectionViewCell {
     
     @IBOutlet weak var specialMomentTitle: UILabel!
     @IBOutlet weak var specialMomentDate: UILabel!
-    @IBOutlet weak var specialMomentsImage: UIImageView!
-    @IBOutlet weak var playerContainerView: UIView!
+    @IBOutlet weak var mediaContainerView: UIView!
     
-    private var player: AVPlayer?
-    private var playerLayer: AVPlayerLayer?
+    private var specialMomentsImage: UIImageView?
+    private var playerViewController: AVPlayerViewController?
     
     override func awakeFromNib() {
         super.awakeFromNib()
         setupUI()
-        setupImageConstraints()
+        setupConstraints()
     }
     
     private func setupUI() {
-        specialMomentsImage.layer.cornerRadius = 10
-        specialMomentsImage.clipsToBounds = true
-        specialMomentsImage.contentMode = .scaleToFill
-        playerContainerView.isHidden = true
+        mediaContainerView.layer.cornerRadius = 10
+        mediaContainerView.clipsToBounds = true
+        mediaContainerView.isHidden = true
+        
+        specialMomentTitle.numberOfLines = 0
+        specialMomentTitle.translatesAutoresizingMaskIntoConstraints = false
+        specialMomentDate.numberOfLines = 0
+        specialMomentDate.translatesAutoresizingMaskIntoConstraints = false
     }
     
-    private func setupImageConstraints() {
-        specialMomentsImage.translatesAutoresizingMaskIntoConstraints = false
+    private func setupConstraints() {
+        mediaContainerView.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
-            specialMomentsImage.widthAnchor.constraint(equalToConstant: 201),
-            specialMomentsImage.heightAnchor.constraint(equalToConstant: 150)
+            mediaContainerView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            mediaContainerView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 0),
+            mediaContainerView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: 0),
+            mediaContainerView.heightAnchor.constraint(equalToConstant: 150),
+            
+            specialMomentTitle.topAnchor.constraint(equalTo: mediaContainerView.bottomAnchor, constant: 5),
+            specialMomentTitle.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            specialMomentTitle.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            
+            specialMomentDate.topAnchor.constraint(equalTo: specialMomentTitle.bottomAnchor, constant: 3),
+            specialMomentDate.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            specialMomentDate.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
         ])
     }
     
     func configure(with milestone: (GrowthMilestone, Date)) {
         let (milestoneDetail, achievedDate) = milestone
         
-        // Set the title and date
         specialMomentTitle.text = milestoneDetail.caption ?? milestoneDetail.title
         let dateFormatter = DateFormatter()
         dateFormatter.dateStyle = .medium
         specialMomentDate.text = dateFormatter.string(from: achievedDate)
         
-        // Show video or image depending on the type of media
         if let videoPath = milestoneDetail.userVideoPath, !videoPath.isEmpty {
             showVideoPlayer(with: URL(fileURLWithPath: videoPath))
         } else if let userImagePath = milestoneDetail.userImagePath, !userImagePath.isEmpty {
-            specialMomentsImage.image = UIImage(contentsOfFile: userImagePath)
-            specialMomentsImage.isHidden = false
-            playerContainerView.isHidden = true
+            showImageView(with: UIImage(contentsOfFile: userImagePath))
         } else {
-            specialMomentsImage.image = UIImage(named: milestoneDetail.image)
-            specialMomentsImage.isHidden = false
-            playerContainerView.isHidden = true
+            showImageView(with: UIImage(named: milestoneDetail.image))
         }
     }
     
+    private func showImageView(with image: UIImage?) {
+        specialMomentsImage?.removeFromSuperview()
+        specialMomentsImage = UIImageView(image: image)
+        guard let specialMomentsImage = specialMomentsImage else { return }
+        
+        specialMomentsImage.layer.cornerRadius = 10
+        specialMomentsImage.clipsToBounds = true
+        specialMomentsImage.contentMode = .scaleAspectFill
+        specialMomentsImage.translatesAutoresizingMaskIntoConstraints = false
+        
+        mediaContainerView.addSubview(specialMomentsImage)
+        
+        NSLayoutConstraint.activate([
+            specialMomentsImage.topAnchor.constraint(equalTo: mediaContainerView.topAnchor),
+            specialMomentsImage.leadingAnchor.constraint(equalTo: mediaContainerView.leadingAnchor),
+            specialMomentsImage.trailingAnchor.constraint(equalTo: mediaContainerView.trailingAnchor),
+            specialMomentsImage.bottomAnchor.constraint(equalTo: mediaContainerView.bottomAnchor)
+        ])
+        
+        mediaContainerView.isHidden = false
+    }
+    
     private func showVideoPlayer(with url: URL) {
-        // Remove previous player if exists
-        player?.pause()
-        playerLayer?.removeFromSuperlayer()
+        playerViewController?.view.removeFromSuperview()
+        playerViewController = AVPlayerViewController()
         
-        // Create and setup the player
-        player = AVPlayer(url: url)
+        guard let playerViewController = playerViewController else { return }
         
-        // Ensure that the player is not nil before proceeding
-        guard let player = player else { return }
+        let player = AVPlayer(url: url)
+        playerViewController.player = player
+        playerViewController.view.translatesAutoresizingMaskIntoConstraints = false
+        playerViewController.view.layer.cornerRadius = 10
+        playerViewController.view.clipsToBounds = true
+        playerViewController.allowsPictureInPicturePlayback = true
+        playerViewController.entersFullScreenWhenPlaybackBegins = true
         
-        // Create an AVPlayerLayer to show the video
-        playerLayer = AVPlayerLayer(player: player)
-        playerLayer?.frame = playerContainerView.bounds
-        playerLayer?.videoGravity = .resizeAspectFill
-        playerContainerView.layer.addSublayer(playerLayer!)
+        mediaContainerView.addSubview(playerViewController.view)
         
-        // Play the video
+        NSLayoutConstraint.activate([
+            playerViewController.view.topAnchor.constraint(equalTo: mediaContainerView.topAnchor),
+            playerViewController.view.leadingAnchor.constraint(equalTo: mediaContainerView.leadingAnchor),
+            playerViewController.view.trailingAnchor.constraint(equalTo: mediaContainerView.trailingAnchor),
+            playerViewController.view.bottomAnchor.constraint(equalTo: mediaContainerView.bottomAnchor)
+        ])
+        player.isMuted = true
         player.play()
         
-        // Hide the image and show the player container view
-        specialMomentsImage.isHidden = true
-        playerContainerView.isHidden = false
+        mediaContainerView.isHidden = false
     }
 }
